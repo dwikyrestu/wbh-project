5b814bb858f901f0271f1f706035e343a25d1081
DESKTOP-95MD4OM\Dwiky Restu <dwikyrestu21@gmail.com>
2018-08-23 18:58:48 +0700

    Detail penjualan read

--- application/views/penjualan/minggu.php
+++ application/views/penjualan/minggu.php
@@ -27,14 +27,14 @@
 </div>
 
 
-<div class="container" id="childMin" style="display:none;">
+<div class="container" id="childMin" style="display:none">
     <!-- Page Heading -->
         <div class="row">
             <h1 class="page-header" id="txt_id">Data Minggu </h1>
         </div>
         <input type="hidden" name="kodemin" id="minkode" value="">
         <div class="row">
-          <div class="pull-left"><a href="#" class="btn btn-sm btn-success" data-toggle="modal" data-target="#ModalaAdd"><span class="fa fa-plus"></span> Tambah Minggu Baru</a></div>
+          <div class="pull-left"><a href="#" class="btn btn-sm btn-success" data-toggle="modal" data-target="#ModalaAdd2"><span class="fa fa-plus"></span> Tambah Minggu Baru</a></div>
         </div><br/>
     <div class="row">
         <div id="reload">
@@ -92,6 +92,48 @@
         <!--END MODAL ADD-->
 
 
+        <!-- MODAL ADD Det Minggu-->
+        <div class="modal" id="ModalaAdd2" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3 class="modal-title" id="myModalLabel">Tambah Data Penjualan</h3>
+            </div>
+            <form class="form-horizontal">
+                <div class="modal-body"><div class="form-group">
+                        <label class="control-label col-xs-3" >Nama Penjualan</label>
+                        <div class="col-xs-9">
+                            <input name="penj" id="nm_penj" class="form-control" type="text" placeholder="Nama Penjualan" style="width:335px;" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-xs-3" >Nilai Penjualan</label>
+                        <div class="col-xs-9">
+                            <input name="nilaipenj" id="nm_nilai" class="form-control" type="number" placeholder="000000000" style="width:335px;" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label col-xs-3" >Person</label>
+                        <div class="col-xs-9">
+                            <input name="person" id="nm_person" class="form-control" type="number" placeholder="000" style="width:335px;" required>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button class="btn" data-dismiss="modal" aria-hidden="true">Tutup</button>
+                    <button class="btn btn-info" id="btn_simpan">Simpan</button>
+                </div>
+            </form>
+            </div>
+            </div>
+        </div>
+        <!--END MODAL ADD Det Minggu-->
+
+
         <!--MODAL HAPUS-->
         <div class="modal fade" id="ModalHapus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
             <div class="modal-dialog" role="document">
@@ -118,15 +160,40 @@
         </div>
         <!--END MODAL HAPUS-->
 
+        <!--MODAL HAPUS Det Minggu-->
+        <div class="modal fade" id="ModalHapus2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Hapus Data Penjualan</h4>
+                    </div>
+                    <form class="form-horizontal">
+                    <div class="modal-body">
+
+                            <input type="hidden" name="kodehps2" id="textkodehps2" value="">
+                            <div class="alert alert-warning"><p>Apakah Anda yakin mau mengapus data penjualan ini?</p></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+                        <button class="btn_hapus btn btn-danger" id="btn_hapus">Hapus</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!--END MODAL HAPUS Det Minggu-->
+
 <script type="text/javascript" src="<?php echo base_url().'asset/js/jquery.js'?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'asset/js/bootstrap.js'?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'asset/js/jquery.dataTables.js'?>"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         tampil_data_barang();   //pemanggilan fungsi tampil barang.
+        //tampil_data_minggu();
 
         $('#mydata').dataTable();
-        $('#mydata2').dataTable();
+        //$('#mydata2').dataTable();
 
         //fungsi tampil barang
         function tampil_data_barang(){
@@ -160,31 +227,30 @@
 
         //function tampil data penjualan perminggu
         function tampil_data_minggu(){
-          var idmin=$('#kodemin').val();
+          var idmin=$('#minkode').val();
             $.ajax({
                 type  : 'ajax',
                 url   : '<?php echo base_url()?>/minggu/data_perminggu/'+idmin,
                 async : false,
                 dataType : 'json',
                 success : function(data){
-                    var html = '';
+                    var html2 = '';
                     var i;
                     var no=1;
                     for(i=0; i<data.length; i++){
-                        html += '<tr>'+
+                        html2 += '<tr>'+
                                 '<td>'+no+'</td>'+
-                                '<td>'+data[i].nama_minggu+'</td>'+
-                                '<td>'+data[i].omset_minggu+'</td>'+
-                                '<td>'+data[i].omset_minggu+'</td>'+
-                                '<td>'+data[i].omset_minggu+'</td>'+
+                                '<td>'+data[i].nama_detpen+'</td>'+
+                                '<td>'+data[i].nilai_detpen+'</td>'+
+                                '<td>'+data[i].person+'</td>'+
                                 '<td style="text-align:right;">'+
-                                    '<a href="javascript:;" class="btn btn-success btn-sm item_det" id="'+data[i].id_minggu+'" data="'+data[i].nama_minggu+'">Detail Minggu</a>'+' '+
-                                    '<a href="javascript:;" class="btn btn-danger btn-sm item_hapus" data="'+data[i].id_minggu+'">Hapus</a>'+
+                                    '<a href="javascript:;" class="btn btn-success btn-sm item_det" id="'+data[i].id_detpen+'" data="'+data[i].nama_minggu+'">Detail Minggu</a>'+' '+
+                                    '<a href="<?php echo base_url(); ?>penjualan/hapus_det/'+data[i].id_detpen+'" class="btn btn-danger btn-sm item_hapus2" data="'+data[i].id_detpen+'">Hapus</a>'+
                                 '</td>'+
                                 '</tr>';
                                 no++;
                     }
-                    $('#show_data2').html(html);
+                    $('#show_data2').html(html2);
                 }
 
             });
@@ -196,6 +262,14 @@
             $('#ModalHapus').modal('show');
             $('[name="kodehps"]').val(id);
         });
+
+        //GET HAPUS
+        $('#show_data').on('click','.item_hapus2',function(){
+            var id=$(this).attr('data');
+            $('#ModalHapus2').modal('show');
+            $('[name="kodehps2"]').val(id);
+        });
+
 
         $('#show_data').on('click','.item_det',function(){
             var nama=$(this).attr('data');
